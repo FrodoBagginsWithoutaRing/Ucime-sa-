0f701281e9b5e155b9db0b5180f2d0c14c7006ae
Dominik Požeský <13domco@gmail.com>
2022-09-14 22:28:04 +0200

    druha uloha hotovo

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("12")
+a=int (input("Zadaj začiatok intervalu: "))
+b=int (input("Zadaj koniec intervalu: "))
+c=float (input("Zadaj čislo ktore chces zistit či patrí do intervalu: "))
+l = list(range(a, b,1 ))
+if c in l:
+    print("tvoje čislo je súčasťou intervalu")
+else:
+    print("tvoje čislo nie je sučasťou intervalu")
+
