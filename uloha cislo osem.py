d4522e5febc09c4d22c8bcc4a7f8e291e5d000f4
FrodoBagginsWithoutaRing <13domco@gmail.com>
2022-09-20 22:51:11 +0200

    ulohy

---
+++ uloha cislo osem.py
@@ -0,0 +1,6 @@
+c=input('Lubovolne cislo: ')
+cp= float(c[-1])+float(c[-2])
+if cp % 2==0:
+    print('Tvoje posledne dvojcislie je parne')
+else:
+    print('Tvoje posledne dvojcislie nie je parne')
---
+++ uloha cislo sest.py
@@ -0,0 +1,5 @@
+a=float(input('Zadaj číslo: '))
+if a % 2 == 0:
+    print('Tvoje čislo je párne.')
+else:
+    print("Tvoje číslo nie je párne.")
